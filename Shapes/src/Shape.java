a8af29e64dcef9ccb678f102e447ede61cc16c0b
emily <emily.m.hill@gmail.com>
2015-09-11 10:05:15 -0400

    Shape interface

---
+++ Shapes/src/Shape.java
@@ -0,0 +1,11 @@
+
+public interface Shape {
+	public double getWidth();
+	public double getHeight();
+	public double getArea();
+	public void setSideLength(double sideLength);
+	public double getPerimeter();
+	public double getInteriorAngle();
+	public int getNumSides();
+	public double getSideLength();
+}
