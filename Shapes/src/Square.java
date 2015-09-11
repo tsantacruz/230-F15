fe0dacc4726e9e2c0e39c26afec79566b8b48640
vallampalli <vishnu.allamp@gmail.com>
2015-09-11 14:02:17 -0400

    Just created the square class

---
+++ Shapes/src/Square.java
@@ -0,0 +1,57 @@
+
+public class Square implements Shape {
+
+	@Override
+	public double getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setSideLength(double sideLength) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double getPerimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getInteriorAngle() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumSides() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getSideLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
