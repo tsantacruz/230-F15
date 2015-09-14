b0b0f1ba2523d7830076986a510499c751d4da09
Chris Thurber <cthurber@me.com>
2015-09-14 09:52:04 -0400

    Added Triangle Class

---
+++ Shapes/src/Triangle.java
@@ -0,0 +1,21 @@
+
+public class Triangle {
+
+	private int height;
+	private int width;
+	
+	//Returns Triangle's Height
+	public int getHeight() {
+		return height;
+	}
+
+	//Returns Triangle's Width
+	public int getWidth() {
+		return width;
+	}
+
+	//Return's the area of the Triangle
+	public double getArea() {
+		return 0.5*width*height;
+	}
+}
