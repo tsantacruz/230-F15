1657982429f6241f34b9a22d662e8f05e4b95327
McLean, Robert <rmclean@drew.edu>
2015-09-14 01:03:56 -0400

    Changed the methods getWidth and getHeight in Circle

--- Shapes/src/Circle.java
+++ Shapes/src/Circle.java
@@ -1,19 +1,19 @@
 
-public class Circle {
-private int numSides;
+public class Circle  {
+
 private int radius;
 	public Circle() {
-		numSides = 0;
+	
 		radius = 2;
 	
 	}
   public double getWidth(){
-	  return radius + 1;
+	  return radius* 2;
 	  
   }
   
   public double getHeight(){
-	  return radius + 1;
+	  return radius *2;
   }
   public static void main(String [] args){
 	  Circle c = new Circle();
