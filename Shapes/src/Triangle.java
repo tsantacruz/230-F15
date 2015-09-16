daeb731d5c7ec57ad073b4449d04ba3b6eb44c68
Chris Thurber <cthurber@me.com>
2015-09-16 09:18:59 -0400

    Updated getWidth, getHeight, getArea methods

--- Shapes/src/Triangle.java
+++ Shapes/src/Triangle.java
@@ -3,29 +3,29 @@ public class Triangle implements Shape{
 
 	private int NumSide();
 	private double sideLength();
-	
+
 	public double getWidth() {
-		
+		return sideLength;
 	};
 	public double getHeight() {
-		
+		return (getArea()/0.5*sideLength());
 	};
 	public double getArea() {
-		
+		return 0.5*setSideLength*3
 	};
 	public void setSideLength(double sideLength) {
-		
+
 	};
 	public double getPerimeter() {
-		
+
 	};
 	public double getInteriorAngle() {
-		
+
 	};
 	public int getNumSides() {
-		
+
 	};
 	public double getSideLength() {
-		
+
 	};
-}
+	}
